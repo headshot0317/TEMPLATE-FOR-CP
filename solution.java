e0adf2fb99f58afd976d00cba500786ab171bd68
Akash Singh Kushwah <77046910+headshot0317@users.noreply.github.com>
2022-07-24 13:33:46 +0530

    Add files via upload

---
+++ solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+class solution{
+public static void main (String[] args) {  		
+     
+////////////////////////////////////////////////////////////////////  	
+out.flush();out.close();
+}//*END OF MAIN METHOD*
+static Map<Integer, Integer> map(int[] a) {
+Map<Integer,Integer> map=new HashMap<>();
+for(int i:a){
+    map.put(i,map.getOrDefault(i,0)+1);
+}
+return map;
+}
+static final Random random = new Random();
+static class FastScanner {
+public long[][] readArrayL;
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+StringTokenizer st=new StringTokenizer("");
+String next() {
+while (!st.hasMoreTokens())
+try {
+st=new StringTokenizer(br.readLine());
+} catch (IOException e) {e.printStackTrace();
+}
+return st.nextToken();
+}
+int nextInt() {
+return Integer.parseInt(next());
+}
+long[] Arrayl(int n) {
+long a[]=new long[n];
+for(int i=0;i<n;i++) a[i]=nextLong();
+return a;
+}
+int[] Array(int n) {
+int[] a=new int[n];
+for (int i=0; i<n; i++) a[i]=nextInt();
+return a;
+}
+long nextLong() {
+return Long.parseLong(next());
+}
+double nextDouble() {
+return Double.parseDouble(next());
+}
+}
+static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+static FastScanner sc = new FastScanner();
+}//*END OF MAIN CLASS*
+
